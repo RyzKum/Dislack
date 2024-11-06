affb000d131ee32cca9198247575ba464df5f7d3
rat-balls <delalande.ethan@gmail.com>
2024-11-06 10:45:42 +0100

    Added friend requests

--- src/pages/Message.tsx
+++ src/pages/Message.tsx
@@ -6,13 +6,26 @@ import { SubmitHandler, useForm } from "react-hook-form";
 import { useStore } from "../utils/store";
 
 type Input = {
-  name: string
+  receiverId: string
+}
+
+type Friend = {
+  userId: string,
+  username: string,
+  startedAt: string
+}
+
+type FriendRequest = {
+  id: string,
+  senderId: string,
+  startedAt: string
 }
 
 function Message() {
   const [messages] = useState<string[]>([]);
   const [input, setInput] = useState("");
-  const [friends, setFriends] = useState<string[]>([]);
+  const [friends, setFriends] = useState<Friend[]>([]);
+  const [friendRequests, setFriendRequests] = useState<FriendRequest[]>([]);
   const currUser = useStore((state) => state.user);
 
   const [hint, setHint] = useState<string>('');
@@ -23,23 +36,38 @@ function Message() {
   } = useForm<Input>();
 
   useEffect(() => {
-    axios.get('http://localhost:3000/social/friends')
+    axios.get('http://localhost:3000/social/friends', { withCredentials: true })
       .then(response => {
         setFriends(response.data);
       })
       .catch(error => {
         console.error("Fetching friends not working ", error);
       });
+    
+    axios.get('http://localhost:3000/social/friend-requests', { withCredentials: true })
+      .then(response => {
+        setFriendRequests(response.data);
+      })
+      .catch(error => {
+        console.error("Fetching friends-requests not working ", error);
+      });
   }, []);
 
-  const addFriend: SubmitHandler<Input> = (data) => {
-    console.log()
-    console.log(data.name)
-    setHint("This user doesn't exist.")
-    axios.get('http://localhost:3000/auth/me').then(res => console.log(res));
-    //get user id by name
-    //run /social/friend-request/{userid}
+  const sendRequest: SubmitHandler<Input> = (data) => {
+    axios.post(`http://localhost:3000/social/friend-request/${currUser.id}`, data, { withCredentials: true })
+      .then(res => console.log("Send friend request: " + res.status))
+      .catch(error => {
+        setHint("Error occurred during friend request.")
+        console.error("Error during friend request", error);
+      });
   };
+
+  function acceptRequest(id: string) {
+    axios.post(`http://localhost:3000/social/friend-request/${id}/accept`, 
+      undefined,
+      { withCredentials: true })
+      .then(res => {console.log("Accept friend request: " + res.status)})
+  }
 
   return (
     <div className="flex min-h-screen">
@@ -47,7 +75,6 @@ function Message() {
       <div className="flex-1 flex">
 
         <div className="w-1/4 bg-gray-200 p-4 overflow-y-auto h-screen flex flex-col justify-between">
-
           <div>
             <h2 className="text-xl font-bold mb-4 sticky top-0 bg-gray-200 p-4">Friends</h2>
             <div>
@@ -56,21 +83,34 @@ function Message() {
                   <div className="w-10 h-10 bg-gray-300 rounded-full mr-4 flex items-center justify-center">
                     <FaUser className="text-gray-500" />
                   </div>
-                  {friend}
+                  {friend.username}
                 </div>
               ))}
             </div>
           </div>
-
-          <form onSubmit={handleSubmit(addFriend)} className="flex flex-col">
-            <p className="m-1">{hint != "" ? <p>{hint}</p> : <></>}</p>
+          <div>
+            <h2 className="text-xl font-bold mb-4 sticky top-0 bg-gray-200 p-4">Friends Requests</h2>
             <div>
-              <input {...register("name", { required: true })} className="p-1 rounded-sm w-4/6" />
+              {
+                friendRequests.map((request, index) => (
+                  <div key={index} className="bg-white p-4 rounded mb-2 shadow flex items-center">
+                    <p>id: {request.id}</p>
+                    <button onClick={() => {acceptRequest(request.id)}}>Accept</button>
+                  </div>
+                ))
+              }
+            </div>
+          </div>
+        
+          <form onSubmit={handleSubmit(sendRequest)} className="flex flex-col">
+            <p className="m-1">{hint != "" ? hint : <></>}</p>
+            <div>
+              <input {...register("receiverId", { required: true })} className="p-1 rounded-sm w-4/6" />
               <input className="ml-3 hover:bg-gray-300 active:bg-gray-400 py-1 px-2 rounded-sm cursor-pointer" type="submit" value="Add friend"/>
             </div>
           </form>
         </div>
-
+        
         <div className="flex-1 bg-gray-100 flex flex-col">
 
           <div className="bg-white p-4 mb-4">
